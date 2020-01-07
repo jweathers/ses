f2b4a2d8ee6c36660a1d5b675d506725753f0a86
Jason Weathers <jason@Jasons-MacBook-Air.local>
2020-01-07 09:30:57 -0600

    initial commit

---
+++ src/SES.Store/IEventStore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SES.Store
+{
+    public interface IEventStore
+    {
+        Task<IEnumerable<byte[]>> Fetch(string queue, ulong startIndex, uint count);
+        Task Store(string queue,byte[] @eventData);
+    }
+}
